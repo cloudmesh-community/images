8f02e0d6a5bc57530ab60a86c0c63884fa38ee3b
Gregor von Laszewski <laszewski@gmail.com>
2018-10-23 12:46:03 -0400

    add vm location for pyenv

---
+++ vm/pyenv/install.sh
@@ -0,0 +1,1 @@
+put your script here, ENV2 and ENV3 must be avialable, and bashrc must be updated. avoid updating bashrc twice, a student did this yesterday, so if pyenv is already in there you can not cincatenate
